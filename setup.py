279bcd89f8e69100aa35faf3534caf5c7d94d850
marc0s <marc0s@fsfe.org>
2010-08-09 17:25:22 +0200

    Version bump to 0.1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_templates():
 
 setup(
     name='Oak',
-    version='0.1.5',
+    version='0.1.6',
     url='http://github.com/marc0s/oak',
     author='marc0s',
     author_email='marc0s@fsfe.org',
